923feb55bfbedf83f339d6292a9d3d78c6e27cb5
LucasNMDS <134799323+LucasNMDS@users.noreply.github.com>
2025-11-26 21:44:22 -0300

    Delete PaleoMap directory

--- PaleoMap/static/JS/mapa/api.js
+++
@@ -1,9 +0,0 @@
-// api.js - Funções de comunicação com o backend
-
-export async function fetchData() {
-    const response = await fetch('/api/dados_fosseis/');
-    if (!response.ok) {
-        throw new Error(`Erro ao comunicar com o servidor: ${response.statusText}`);
-    }
-    return await response.json();
-}
